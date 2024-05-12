87857f7262191f4c5b8fcfb63c4c12c575ccde6c
tbjuechen <1324374092@qq.com>
2024-05-12 19:17:19 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+from DrissionPage import ChromiumPage
+
+# 创建页面对象，并启动或接管浏览器
+page = ChromiumPage()
+# 跳转到登录页面
+page.get('https://www.xiaohongshu.com/explore')
+
+# 定位到账号文本框，获取文本框元素
+ele = page.ele('#user_login')
+# 输入对文本框输入账号
+ele.input('您的账号')
+# 定位到密码文本框并输入密码
+page.ele('#user_password').input('您的密码')
+# 点击登录按钮
+page.ele('@value=登 录').click()
