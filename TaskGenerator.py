bc843a923b2700867e2e6591dc17e7be2d4c45fb
tbjuechen <1324374092@qq.com>
2024-05-13 13:53:23 +0800

    切换穿搭分区

--- TaskGenerator.py
+++ TaskGenerator.py
@@ -1,6 +1,8 @@
 from DrissionPage import ChromiumPage
 import config
 
+class ElementNotFound(Exception):...
+
 class TaskGenerator(object):
     def __init__(self) -> None:
         self.start_page = config.STARTPAGE
@@ -9,18 +11,32 @@ class TaskGenerator(object):
     def head_page_init(self):
         self.broswer.get(self.start_page)
         self.close_login_box()
+        self.turn_to_dressing_category()
 
     def close_login_box(self):
-        # 查找 .login—container 元素
-        login_container_ele = self.broswer.ele('.login-container', timeout= 2)
+        '''
+        关闭登录框
+        '''
+        # 查找 .login—container
+        login_container_ele = self.broswer.ele('.login-container', timeout=2)
         # 存在登录框则关闭
         if login_container_ele:
             login_container_close_button_ele = login_container_ele.child('tag:div')
             login_container_close_button_ele.click()
-
+    
+    def turn_to_dressing_category(self):
+        '''
+        切换到穿搭分区
+        '''
+        # 查找 .content-container
+        navigation_bar = self.broswer.ele('.content-container', timeout=2)
+        if navigation_bar:
+            dressing_button_ele = navigation_bar.child('tag:div', index=3)
+            dressing_button_ele.click()
+        else:
+            raise ElementNotFound('navidation bar not found')
 
 # 测试
 if __name__=='__main__':
     tg = TaskGenerator()
     tg.head_page_init()
-    tg.close_login_box()
